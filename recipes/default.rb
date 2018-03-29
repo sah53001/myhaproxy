28bdd16472f84e16ce7daf82b392adb4adbd7ba5
Stephen.Heywood <Stephen.Heywood@caremark.com>
2018-03-29 14:33:55 -0700

    First commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] =
+  [
+    {
+      'hostname' => 'ec2-35-153-99-74.compute-1.amazonaws.com',
+      'ipaddress' => '35.153.99.74',
+      'port' => 80,
+      'ssl_port' => 80
+    }
+  ]
+
+include_recipe 'haproxy::manual'

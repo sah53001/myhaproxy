d544ae41e2a6146d0ec66b98f5f29cccda8b2425
Stephen.Heywood <Stephen.Heywood@caremark.com>
2018-03-30 10:00:48 -0700

    Version 1.0.0: dynamic update load balancing pool

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,19 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-node.default['haproxy']['members'] =
-  [
-    {
-      'hostname' => 'ec2-35-153-99-74.compute-1.amazonaws.com',
-      'ipaddress' => '35.153.99.74',
-      'port' => 80,
-      'ssl_port' => 80
-    }, {
-      'hostname' => 'ec2-34-229-23-70.compute-1.amazonaws.com',
-      'ipaddress' => '34.229.23.70',
-      'port' => 80,
-      'ssl_port' => 80
-    }
-  ]
+
+all_web_nodes = search('node', 'role:web_server')
+members = []
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
+    'port' => 80,
+    'ssl_port' => 80
+  }
+  members.push(member)
+end
+
+node.default['haproxy']['members'] = members
 
 include_recipe 'haproxy::manual'
